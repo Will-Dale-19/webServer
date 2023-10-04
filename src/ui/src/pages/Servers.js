dd8c166e9fc321d1de0cdadfd20bbf1382575a56
will dale <dalew@msoe.edu>
2023-10-04 10:36:23 -0500

    moved the buttons to a component file to reduce clutter within Servers.js

--- src/ui/src/pages/Servers.js
+++ src/ui/src/pages/Servers.js
@@ -1,5 +1,5 @@
 import React, { useLayoutEffect, useState } from 'react'
-import styled from "styled-components";
+import Buttons from "../components/Buttons";
 
 const Servers = () => {
     const [servers, setServers] = useState([])
@@ -14,8 +14,6 @@ const Servers = () => {
             console.log('error fetching customers: ' + e);
         })
     })
-
-
 
     return (
         <div>
@@ -42,80 +40,11 @@ const Servers = () => {
             })}
             </tbody>
         </table>
-            <div><Button onClick={clickMe}>Button</Button></div>
-            <div><ToggleGroup /></div>
+            <Buttons />
     </div>
 
     )
 
-}
-
-const theme = {
-    blue: {
-        default: "#3f51b5",
-        hover: "#283593"
-    }
-};
-
-const Button = styled.button`
-  background-color: ${(props) => theme[props.theme].default};
-  color: white;
-  padding: 5px 15px;
-  border-radius: 5px;
-  outline: 0;
-  text-transform: uppercase;
-  margin: 10px 0px;
-  cursor: pointer;
-  box-shadow: 0px 2px 2px lightgray;
-  transition: ease background-color 250ms;
-  &:hover {
-    background-color: ${(props) => theme[props.theme].hover};
-  }
-  &:disabled {
-    cursor: default;
-    opacity: 0.7;
-  }
-`;
-
-Button.defaultProps = {
-    theme: "blue"
-};
-
-const ButtonToggle = styled(Button)`
-  opacity: 0.7;
-  ${({ active }) =>
-    active &&
-    `
-    opacity: 1; 
-  `}
-`;
-
-
-const types = ["Start Server", "Stop Server"];
-
-function ToggleGroup() {
-    const [active, setActive] = useState(types[0]);
-    return (
-        <div>
-            {types.map((type) => (
-                <ButtonToggle active={active === type} onClick={()=> {
-                    if (type === types[0]) {
-                        setActive(types[1])
-                    } else {
-                        setActive(types[0])
-                    }
-                    }
-                }>
-
-                    {type}
-                </ButtonToggle>
-            ))}
-        </div>
-    );
-}
-
-function clickMe() {
-    alert("You clicked me!");
 }
 
 export default Servers
